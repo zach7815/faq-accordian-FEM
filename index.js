da6301c27dbc104277f67d2dc6d6143c6111033d
zach7815 <53745970+zach7815@users.noreply.github.com>
2021-10-31 21:32:34 +0000

    added Javascript. Changed HTML to deal with a layout problem at desktop, mobile layout needs fixing again.

---
+++ index.js
@@ -0,0 +1,22 @@
+alert("js is connected")
+
+const acc = document.querySelectorAll(".accordian");
+
+
+
+for(let i=0; i<acc.length;i++){
+
+acc[i].addEventListener("click", function(){
+   this.classList.toggle("active");
+   let panel = this.nextElementSibling;
+if(panel.style.maxHeight){
+    panel.style.maxHeight=null;
+}
+else{
+    panel.style.maxHeight=panel.scrollHeight+ "px";
+}
+
+})
+
+
+}
