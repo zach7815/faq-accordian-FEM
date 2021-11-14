674ac09fd81fa55c154027934ed29a915b9f128b
zach7815 <53745970+zach7815@users.noreply.github.com>
2021-11-14 20:08:45 +0000

    made design responsive for all sizes. Completed JS interactivity. Project complete

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-alert("js is connected")
+const acc= document.querySelectorAll(".accordian");
+const downArrow = document.querySelectorAll(".downArrow");
 
-const acc = document.querySelectorAll(".accordian");
-
-
-
-for(let i=0; i<acc.length;i++){
-
-acc[i].addEventListener("click", function(){
-   this.classList.toggle("active");
-   let panel = this.nextElementSibling;
-if(panel.style.maxHeight){
-    panel.style.maxHeight=null;
-}
-else{
-    panel.style.maxHeight=panel.scrollHeight+ "px";
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        hideAll();
+        this.classList.add("active");
+        downArrow[i].classList.add("rotate");
+        this.nextElementSibling.style.maxHeight= this.scrollHeight +"px";
+    })   
 }
 
-})
-
-
+function hideAll() {
+    for (i = 0; i < acc.length; i++) {
+        acc[i].classList.remove("active");
+        downArrow[i].classList.remove("rotate");
+        acc[i].nextElementSibling.style.maxHeight="0";
+    }
 }
+
+
